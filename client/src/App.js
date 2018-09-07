45637f6118b54c489889f7267813a4de99e6d994
crypto-jones <integral.being@gmail.com>
2018-09-06 19:46:29 -0700

    implement username form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import UsernameForm from './components/UsernameForm';
 import './App.css';
 
 class App extends Component {
+  state = {
+    currentUsername: ''
+  };
+
+  onUsernameSubmitted = username => {
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username })
+    })
+      .then(response => {
+        this.setState({
+          currentUsername: username
+        });
+      })
+      .catch(error => console.error('error', error));
+  };
+
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <h1>Chat App</h1>
+        <UsernameForm onSubmit={this.onUsernameSubmitted} />
+      </div>
+    );
   }
 }
 
