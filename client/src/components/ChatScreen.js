a22c9a254eb8a6ef230d2dd2fd2e819c1e58d17b
crypto-jones <integral.being@gmail.com>
2018-09-06 20:34:35 -0700

    connect to Chatkit instance

--- client/src/components/ChatScreen.js
+++ client/src/components/ChatScreen.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
+import Chatkit from '@pusher/chatkit';
+import INSTANCE_LOCATER from '../credentials';
 
 class ChatScreen extends Component {
+  state = {
+    currentUser: {}
+  };
+
+  componentDidMount = () => {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: INSTANCE_LOCATER,
+      userId: this.props.currentUsername,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: 'http://localhost:5000/authenticate'
+      })
+    });
+
+    chatManager
+      .connect()
+      .then(currentUser => {
+        this.setState({ currentUser });
+      })
+      .catch(error => console.error('error', error));
+  };
+
   render() {
     return (
       <div>
