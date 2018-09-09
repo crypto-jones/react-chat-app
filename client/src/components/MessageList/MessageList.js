cdca1643d14fb91bf35c1f8297f53a87bd932280
crypto-jones <integral.being@gmail.com>
2018-09-08 20:52:29 -0700

    integrate scroll to bottom functionality

--- client/src/components/MessageList/MessageList.js
+++ client/src/components/MessageList/MessageList.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import * as ReactDOM from 'react-dom';
 import './MessageList.css';
 
 class MessagesList extends Component {
+  scrollToBottom = () => {
+    const { messageList } = this.refs;
+    const scrollHeight = messageList.scrollHeight;
+    const height = messageList.clientHeight;
+    const maxScrollTop = scrollHeight - height;
+    ReactDOM.findDOMNode(messageList).scrollTop =
+      maxScrollTop > 0 ? maxScrollTop : 0;
+  };
+
+  componentDidUpdate = () => {
+    this.scrollToBottom();
+  };
+
   render() {
     return (
-      <div className="message-container">
+      <div className="message-container" ref="messageList">
         <ul className="users-list">
           {this.props.messages.map((message, index) => (
             <li key={index} className="user-posts">
